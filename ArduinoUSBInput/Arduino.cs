4b7376e3871890e4547c4c48d50cb3ebf3807e1e
NeilOCV <31700601+NeilOCV@users.noreply.github.com>
2017-09-20 06:06:25 +0200

    First check in

---
+++ ArduinoUSBInput/Arduino.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ArduinoUSBInput
+{
+    public class ButtonStateEventArgs:EventArgs
+    {
+        public string ButtonState { get; set; }
+    }
+    public class Arduino
+    {
+        string strLastState = "";
+        public delegate void ButtonHandler(object sender, ButtonStateEventArgs e);
+        public event ButtonHandler ButtonChanged;
+        private SerialPort port;
+        public int BaudRate { get; set; }
+        public string PortName { get; set; }
+        public Arduino()
+        {
+            port = new SerialPort();
+            BaudRate = 9600;
+            PortName = "COM5";
+            port.PortName = PortName;
+            port.BaudRate = BaudRate;
+            port.Open();
+        }
+        ~Arduino()
+        {
+            port.Close();
+        }
+        public void Listen()
+        {
+            string strPortState = port.ReadLine().Trim();
+            if(strLastState != strPortState)
+            {
+                strLastState = strPortState;
+                RaiseEvent(strLastState);
+            }
+            
+        }
+        protected virtual void RaiseEvent(string strButtonState)
+        {
+
+            if (ButtonChanged != null)
+                if (strButtonState.Trim() == "Closed")
+                    ButtonChanged(this, new ButtonStateEventArgs { ButtonState = "Closed" });
+                else
+                    ButtonChanged(this, new ButtonStateEventArgs { ButtonState = "Open" });
+     
+
+        }
+    }
+}
+
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO.Ports;
+
+
+
+namespace ArduinoWeb
+{
+    public enum ValueToSend
+    {
+        sendOFF = 0, sendRed = 1, sendGreen = 2, sendBoth = 3
+    }
+    public class Arduino
+    {
+        private SerialPort port;
+        public int BaudRate { get; set; }
+        public string PortName { get; set; }
+        public Arduino()
+        {
+            port = new SerialPort();
+            BaudRate = 9600;
+            PortName = "COM3";
+        }
+        public void Send(ValueToSend value)
+        {
+            switch (value)
+            {
+                case ValueToSend.sendOFF:
+                    {
+                        BaudRate = BaudRate;
+                        port.PortName = PortName;
+                        port.Open();
+                        port.WriteLine("0");
+                        port.Close();
+                    }
+                    break;
+                case ValueToSend.sendGreen:
+                    {
+                        BaudRate = BaudRate;
+                        port.PortName = PortName;
+                        port.Open();
+                        port.WriteLine("2");
+                        port.Close();
+                    }
+                    break;
+                case ValueToSend.sendRed:
+                    {
+                        BaudRate = BaudRate;
+                        port.PortName = PortName;
+                        port.Open();
+                        port.WriteLine("1");
+                        port.Close();
+                    }
+                    break;
+                case ValueToSend.sendBoth:
+                    {
+                        BaudRate = BaudRate;
+                        port.PortName = PortName;
+                        port.Open();
+                        port.WriteLine("3");
+                        port.Close();
+                    }
+                    break;
+                default:
+                    {
+                        port.Open();
+                        port.WriteLine("0");
+                        port.Close();
+                    }
+                    break;
+            }
+        }
+
+    }
+}
+*/
---
+++ ArduinoUSBInput/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoUSBInput
+{
+    public partial class Form1 : Form
+    {
+        
+        Arduino arduino = new Arduino();
+        System.Threading.Thread ArduinoThread;
+
+        ~Form1()
+        {
+            arduino = null;
+            ArduinoThread.Abort();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            arduino.ButtonChanged += arduino_ButtonChanged;
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        void arduino_ButtonChanged(object sender, ButtonStateEventArgs e)
+        {
+            //MessageBox.Show(e.ButtonState);
+            
+            lblStateChanges.Items.Add(DateTime.Now.ToShortDateString() + " - " + lblStateChanges.Items.Count.ToString() + ") " + e.ButtonState);
+
+
+            
+            DoListen();
+        }
+
+        private void DoListen()
+        {
+            ArduinoThread = new System.Threading.Thread(arduino.Listen);
+            ArduinoThread.Start();
+            //Application.DoEvents();
+            //arduino.Listen();
+            //Application.DoEvents();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DoListen();
+        }
+
+        private void lblStateChanges_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
